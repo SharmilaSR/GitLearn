81acb6681daf2f73e7fbe5f7581d5a2a2783ae25
SharmilaSR <sharmila124@yahoo.com>
2023-02-03 16:09:30 +0000

    first commit

---
+++ src/test1.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+
+public class test1 {
+	
+	public void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<String> names = new ArrayList<String>();
+		names.add("Anna");
+		names.add("Don");
+		names.add("Abigail");
+		names.add("Adam");
+		names.add("Sharmila");
+		int count = 0;
+		
+		for(int i=0; i<names.size(); i++) {
+			
+			String actual = names.get(i);
+			if(actual.startsWith("A")) {
+				count++;
+			}
+			System.out.println(count);
+		}
+	}
+
+}
